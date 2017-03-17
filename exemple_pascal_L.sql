76d0c94f3cf557762dcabb16724d6e3cf4a433e6
Sylvain Montagner <sig-pnrnm@users.noreply.github.com>
2017-03-17 16:37:23 +0100

    Create exemple_pascal_L.sql

---
+++ exemple_pascal_L.sql
@@ -0,0 +1,36 @@
+/* REINTRODUCTION des données d’une commune DE LA TABLE GEO_P vers LA TABLE GEO_T */
+/* changer le code insee 60589 par le bon code insee*/
+/* ****************************************** */
+
+INSERT INTO
+        mon_schema.ma_table_t
+        (
+        idzone,
+        libelle,
+        libelong,
+        typezone,
+        destdomi,
+        l_observ,
+        nomfic,
+        urlfic,
+        insee,
+        datvalid,
+        geom
+        )
+    (
+    SELECT
+        mon_schema.ma_table_p.idzone,
+       mon_schema.ma_table_p.libelle,
+        mon_schema.ma_table_p.libelong,
+        mon_schema.ma_table_p.typezone,
+        mon_schema.ma_table_p.destdomi,
+       mon_schema.ma_table_p.l_observ,
+        mon_schema.ma_table_p.nomfic,
+        mon_schema.ma_table_p.urlfic,
+        mon_schema.ma_table_p.insee,
+        mon_schema.ma_table_p.datvalid,
+        mon_schema.ma_table_p.geom
+    FROM
+       mon_schema.ma_table_p
+    WHERE
+ma_table_p.insee = '60589');
