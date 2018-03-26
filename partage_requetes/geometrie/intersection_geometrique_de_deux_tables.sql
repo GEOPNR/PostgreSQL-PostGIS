e2b3bde7a6fb2ab1c8b1db5c119f5985dffcd598
ybuthion <yann.buthion@pnr-vercors.fr>
2018-03-26 08:47:39 +0200

    Create intersection_geometrique_de_deux_tables.sql

---
+++ partage_requetes/geometrie/intersection_geometrique_de_deux_tables.sql
@@ -0,0 +1,9 @@
+
+--utilisation de st_intersection
+--on créer la table résultat, on sélectionne d'abord les colonnes à préserver dans la table résultante,
+--on lance l'intersection entre les colonnes géométries (les géom doivent de même EPSG)
+
+create table monSchema.MaTableRésultat as
+select matable1.macolonne, matable2.macolonne, st_intersection(matable1.macolonne_geometrie, matable2.macolonne_geometrie) as MaNouvelleColonneGeometrie
+from monschema1.matable1 join monschema2.matable2
+on st_intersects(matable1.macolonne_geometrie, matable2.macolonne_geometrie)
