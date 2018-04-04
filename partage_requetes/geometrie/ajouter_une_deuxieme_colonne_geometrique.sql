1dce250a7c19f797ec30302ab4377244f403b03f
ybuthion <yann.buthion@pnr-vercors.fr>
2018-04-04 16:10:01 +0200

    Create ajouter_une_deuxieme_colonne_geometrique.sql

---
+++ partage_requetes/geometrie/ajouter_une_deuxieme_colonne_geometrique.sql
@@ -0,0 +1,6 @@
+--ajouter une colonne de géométrie en spécifiant le type de géom (multipolygon, polygon, linestrig, ...) et le SRID attendu
+alter table matable add column MaNouvelleColonneDeGeometrie geometry(TypeDeGeometrie, MonSRIDcible);
+update matable set MaNouvelleColonneDeGeometrie = st_transform(MaColonneDeGeometrieActuelle, MonSRIDactuel);
+-- eventuellement un index sur cette colonne si elle est utilisée dans une clause where, ou dans qgis:
+create index matable_MaNouvelleColonneDeGeometrie_gist on matable USING GIST (MaNouvelleColonneDeGeometrie);
+VACUUM ANALYZE matable;
